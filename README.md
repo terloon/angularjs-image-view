54f8d9e6f6e11bde2a8bb69f504af654708ad08e
Terence Ramos <terence.ramos@zerve.com>
2015-04-15 19:08:45 -0700

    Added comments. Added an "on-img-load" event handler. Updated example.

--- README.md
+++ README.md
@@ -6,12 +6,14 @@ This is an angular directive used to display images with some nice attributes us
 Currently images are lazy loaded. Example:
 
 ```
-<div img-src="the-image.jpg" ratio="4:3" fade-in="2"></div>
+<div img-src="the-image.jpg" ratio="4:3" fade-in="2" on-img-load="onLoad()"></div>
 ```
 
 * **aspect-ratio** - Control the aspect-ratio of the image to be displayed. Values like, "4:3", "16:9", "1:1". If no ratio is given, it will be the size of the image.
 
 * **fade-in** - If present, will fade in the image using CSS3 transition effects. This is used in the css to place the transition time. Valid values are floating point numbers.
+
+* **on-img-load** - Can add function handler for when an image has finished loading.
 
 An example can be seen at:
 http://gamecodemonkey.com/angular/image-view/
--- js/image-widgets.js
+++ js/image-widgets.js
@@ -4,13 +4,16 @@
             return {
                 restrict: 'A',
                 scope: {
-                    imgSrc: '@'
+                    imgSrc: '@',
+                    onImgLoad: '&'
                 },
                 link: function(scope, element, attrs) {
                     if (!angular.isDefined(attrs.aspectRatio)) attrs.aspectRatio = '0:0';
+
                     var ratioVals = attrs.aspectRatio.split(':');
 
                     if (ratioVals.length != 2) return;
+
                     var ratioW = parseInt(ratioVals[0]);
                     var ratioH = parseInt(ratioVals[1]);
 
@@ -19,6 +22,7 @@
                     var pad = ((ratioH / ratioW) * 100).toFixed(2);
                     var img = angular.element('<img />');
 
+                    // Center the image within the element container.
                     var center = function() {
                         var cw = element[0].clientWidth;
                         var ch = element[0].clientHeight;
@@ -33,11 +37,13 @@
                         angular.element(img).css('top', top + 'px');
                     };
 
+                    // Initialize the element css.
                     var elementCss = {
                         'position': 'relative',
                         'overflow': 'hidden'
                     };
 
+                    // Initialize the image css.
                     var imgCss = {
                         'display': 'none',
                         'top': '0',
@@ -47,6 +53,9 @@
                         'opacity': '0',
                     };
 
+                    // Check if we have a valid padding value and add it to the
+                    // element css to provide proper aspect-ratio size for the
+                    // container.
                     if (!isNaN(pad)) {
                         elementCss['padding-top'] = pad + '%';
                     } else {
@@ -54,8 +63,11 @@
                         imgCss['height'] = '100%';
                     }
 
+                    // Set the element css.
                     angular.element(element).css(elementCss);
 
+                    // Add CSS3 transition attributes if fade-in is defined and
+                    // has a valid value.
                     if (angular.isDefined(attrs.fadeIn)) {
                         var fadeIn = parseFloat(attrs.fadeIn);
                         if (!isNaN(fadeIn)) {
@@ -68,21 +80,36 @@
                         }
                     }
 
+                    // Set the image css.
                     img.css(imgCss);
                     img.attr('src', scope.imgSrc);
 
+                    // Add handler for when image has finished loading.
                     img.on('load', function() {
-                        if (!isNaN(pad)) {
-                            center();
-                        }
+                        // If there is a valid padding, center the image.
+                        !isNaN(pad) && center();
+
+                        // Display the image.
                         img.css('display', 'block');
+
+                        // So that the transition animation is triggered, we
+                        // need to separate the display attribute change from
+                        // the opacity change.
                         $interval(function() {
                             img.css('opacity', 1);
                         }, 100, 1);
+
+                        // Call the user defined on load function.
+                        angular.isDefined(scope.onImgLoad) && scope.onImgLoad();
                     });
 
+                    // Append the image to the element. This should start the
+                    // loading of the image.
                     element.append(img);
 
+                    // If there is an aspect-ratio no the image, make sure to
+                    // add a handler for when the browser is resized. This will
+                    // appropriately re-center the image within its container.
                     if (!isNaN(pad)) {
                         angular.element($window).bind('resize', function() {
                             center();
